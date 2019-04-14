6fdd9373ac8557ef037319b8c7963c8cc41033b3
David Wightman <david@davidwightman.org>
2019-04-14 15:48:36 -0400

    finished working through quick sort

--- s16l93/index.js
+++ s16l93/index.js
@@ -1,4 +1,4 @@
-function pivot (arr, startIndex = 0, endIndex = arr.length + 1) {
+function pivot(arr, startIndex = 0, endIndex = arr.length - 1) {  
   //designate an element as the pivot
   //rearange elements in the array so that all values less tahn the pivot are moved to the left of the pivot and all values greater than the pivot are moved to the right of the pivot
   // order of elements on either side of the pivot doesn't matter
@@ -8,12 +8,13 @@ function pivot (arr, startIndex = 0, endIndex = arr.length + 1) {
   // it will help to accept three arguments: an array, a start index, and an end index (these can default to 0 and the array length minus 1, respectively)
   // grab the pivot from the start of the array
   //store the current pivot index in a variable (this will keep track of where the pivot should end up)
-  let currentPivotIndex = 0
+  let pivot = arr[startIndex]
+  let currentPivotIndex = startIndex
   // loop throught the array from the start until the end
-  for (i = 1; i < arr.length; i++) {
+  for (i = startIndex + 1; i <= endIndex; i++) {
   // - if the pivot is greatern than the current element, increment the pivot index variable 
   //   and then swap the current element with the element at the pivot index
-    if (arr[startIndex] > arr[i]){
+    if (pivot > arr[i]){
       currentPivotIndex++
       [arr[currentPivotIndex], arr[i]] = [arr[i], arr[currentPivotIndex]];
     }
@@ -22,8 +23,16 @@ function pivot (arr, startIndex = 0, endIndex = arr.length + 1) {
   [arr[startIndex], arr[currentPivotIndex]] = [arr[currentPivotIndex], arr[startIndex]];
   // return the pivot index
   return currentPivotIndex
- 
 
 }
 
-console.log(pivot([28,41,4,11,16,1,40,14,36]))
+function quickSort (arr, left = 0 , right = arr.length - 1) {
+  if (left < right){
+    let pivotIndex = pivot(arr, left, right)
+    quickSort(arr, left, pivotIndex - 1);
+    quickSort(arr, pivotIndex + 1, right);
+  }
+  return arr;
+}
+
+console.log(quickSort([28,41,4,11,16,1,40,14,36]))
